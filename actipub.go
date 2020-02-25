e49fe502a5e6e5e243f02a03ea33d9b9e5f20432
CC <cerisara@loria.fr>
2020-02-25 13:23:00 +0100

    ok download actipub work on android. But formats of posts differs from masto/plemora/... TODO: robust parsing

--- actipub.go
+++ actipub.go
@@ -2,11 +2,10 @@ package main
 
 import (
     "fmt"
-    "bufio"
+    "strings"
         "net/http"
         "io/ioutil"
         "encoding/json"
-        "golang.org/x/mobile/asset"
 )
 
 func getAPNote(url string) string {
@@ -21,6 +20,10 @@ func getAPNote(url string) string {
 func aptest() []string {
     fmt.Println("detson in aptest")
     var ss []string
+    u := "https://bctpub.duckdns.org/polson/outbox?page=1"
+
+    /*
+    * this does not work on android, may be because fyne.io uses a vendor/ dir, which creates 2 contexts in android and thus no asset manager
     f, err := asset.Open("url.txt")
     fmt.Printf("detson %v %v\n",f, err)
     if err != nil {
@@ -29,22 +32,33 @@ func aptest() []string {
     }
     fmt.Println("detson f ok")
     defer f.Close()
-    u := "https://bctpub.duckdns.org/polson/outbox?page=1"
     scanner := bufio.NewScanner(f)
     for scanner.Scan() {
         // only keep the last url
         u = scanner.Text()
     }
+    */
     fmt.Println("detson url: "+u)
 
     s := getAPNote(u)
     var jsonobj map[string]interface{}
     json.Unmarshal([]byte(s), &jsonobj)
+
     z := jsonobj["orderedItems"].([]interface{})
     for _,x := range z {
         y := x.(map[string]interface{})
         u := y["object"].(map[string]interface{})
-        ss=append(ss, u["content"].(string))
+        msg := u["content"].(string)
+        /*
+        date := u["published"].(string)
+        layout := "2006-01-02T15:04:05.000Z"
+        date,_ = time.Parse(layout,date)
+        */
+        from := u["attributedTo"].(string)
+        stmp := strings.Split(from,"/")
+        from = stmp[len(stmp)-1]
+        oneline := from+": "+msg
+        ss=append(ss, oneline)
     }
     return ss
 }
